cd8523235f217b5e7499bdc64c84707c3adf806b
Clayton  J. Wilkerson <clay2000lbs@gmail.com>
2021-11-08 16:33:03 -0600

    inital commit

---
+++ sql-intro/update.sql
@@ -0,0 +1,13 @@
+-- In the update.sql file, write out the code for the following problems:
+
+-- These questions reference both the customer table and track table.
+
+-- Find all customers with fax numbers and set those numbers to null.
+
+-- Find all customers with no company (null) and set their company to “Self”.
+
+-- Find the customer Julia Barnett and change her last name to Thompson.
+
+-- Find the customer with this email luisrojas@yahoo.cl and change his support rep to 4.
+
+-- Find all tracks that are the genre Metal and have no composer. Set the composer to “The darkness around us”.
